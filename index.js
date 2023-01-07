2866aa26d086a778e1ba835d0680d24a2b10767b
Alexmaltais <alexmaltais.1@gmail.com>
2023-01-07 17:56:44 -0500

    ignored file

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const contactRouter = require('./dist/routes/contactRouter');
 mongoose
     .connect('mongodb://localhost/CRMdb', { useNewUrlParser: true })
     .then(() => {
-        console.log('MongoDB Connected in branch push test');
+        console.log('MongoDB Connected in branch push test ignored');
     })
     .catch(err => {
         console.log(err);
-        console.log('MongoDB Not Connected in branch push test');
+        console.log('MongoDB Not Connected in branch push test ignored');
     });
     
 
